cc2d80b79d7cea037487f0b6c90613dad7b07e29
8rdownes <31481017+8rdownes@users.noreply.github.com>
2018-04-10 09:20:12 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -1,8 +1,12 @@
 //Task 1
 function largest(list){
-
+  let largernumber = list[0];
+  for(let a=1; a<list.length; a++){
+    if(list[a]>largernumber){
+      return largernumber;
+    }
+  }
 }
-
 //Task 2
 function reverse(list){
 
@@ -15,7 +19,9 @@ function cotains(list,e){
 
 //Task 4
 function oddElements(list){
+for (let a=1; a<a.length; a=a+2){
 
+}
 }
 
 //Task 5
@@ -46,7 +52,7 @@ function weave(listA, listB){
 //Task 10
 //Attempt last
 function mergeInOrder(listA, listB){
-    
+
 }
 
 //Task 11
