2ea9701702f44167f5cc5aa213b37db4752671ac
8rdownes <31481017+8rdownes@users.noreply.github.com>
2018-04-11 10:06:41 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -3,13 +3,13 @@ function largest(list){
   let largernumber = list[0];
   for(let a=1; a<list.length; a++){
     if(list[a]>largernumber){
-      return largernumber;
+      return list[a];
     }
   }
 }
 //Task 2
 function reverse(list){
-
+let reverselist = list[];
 }
 
 //Task 3
@@ -19,21 +19,29 @@ function cotains(list,e){
 
 //Task 4
 function oddElements(list){
-for (let a=1; a<a.length; a=a+2){
-
-}
+let oddlist = [];
+for (let a=1; a<list.length; a=a+2){
+  if(a%2!=1){
+      oddlist.push(list[a]);
+        }
+     }
 }
 
 //Task 5
 function total(list){
-
+let totalnumbers =[];
+for (let a=1; a<list.length; a++;){
+     let newA= a;
+     totalnumbers.push();
+}
 }
 
 //Task 6
 function isPalindrome(list){
-
+let palindrome =list[0];
+for (let a=1; a<list.length; a++;){
 }
-
+}
 //Task 7
 //Do second to last
 function recursiveTotal(list){
